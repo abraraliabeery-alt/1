f97576e1ae310e3fe6f96e3c5536d87f5b7228ee
Abrar-Ali123 <82758512+Abrar-Ali123@users.noreply.github.com>
2025-12-02 12:19:21 +0300

    Remove favicon tags from layouts

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name','شركة مدى الذهبية') }}</title>
-    <link rel="icon" href="{{ $favicon }}" />
     @vite(['resources/css/app.css','resources/js/app.js'])
   </head>
   <body class="bg-slate-50 text-slate-800 font-sans">
